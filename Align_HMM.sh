64eebb17d0d3628d0c73d45ade20aeefedc86fc1
bcoggins <bcoggins@nd.edu>
2018-12-04 21:06:22 -0500

    cleaned directory and added python plot and script

---
+++ Align_HMM.sh
@@ -0,0 +1,31 @@
+###Usage: bash Align_HMM.sh
+###run after scp to remote: scp transcript*.fasta, scp *
+
+#Make muscle alignment for 6 transcripts
+for sequence in Transcript*_Proteins.fasta
+do
+../../local/bin/muscle3.8.31_i86linux64 -in $sequence -out $sequence.aligned
+echo Aligned
+done
+
+#Make HMM protein models for 6 transcripts
+for aligned in Transcript*_Proteins.fasta.aligned
+do
+../../local/bin/hmmbuild $aligned.hmm $aligned
+echo HMM built
+done
+
+#Search 4 RNAseq files for each of 6 HMMs
+for RNAseq in *
+do
+	for HMM in Transcript*_Proteins.fasta.aligned.hmm
+	do
+	echo $RNAseq >> HMMsearchHits.txt
+	echo $HMM >> HMMsearchHits.txt
+	../../local/bin/hmmsearch --tblout $HMM.txt $HMM $RNAseq
+	grep -v "#" $HMM.txt | wc -l >> HMMsearchHits.txt
+	echo RNAseq files searched for HMMs
+	done
+done
+
+
